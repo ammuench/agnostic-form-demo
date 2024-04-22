6e8a71ceef27f1bde7f493716cc98cb89b28775a
Alex Muench <ammuench@gmail.com>
2024-04-21 19:25:35 -0500

    Create dynamic type and error parsing to support it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { z } from "zod";
 import "./App.css";
 import { ExampleOne } from "./components/ExampleOne";
 import { FormStateDisplay } from "./components/FormStateDisplay";
 import { SubmitSection } from "./components/SubmitSection";
+import { parseDynamicErrors } from "./util/parseZodErrors";
 
 // const fromExampleRenderer = (exampleToUse: FormExampleEnum): JSX.Element => {
 //   switch (exampleToUse) {
@@ -11,6 +13,29 @@ import { SubmitSection } from "./components/SubmitSection";
 // };
 
 const App = () => {
+  try {
+    const testArrSchema = z.object({
+      testArrFoo: z.string(),
+      testArrBar: z.string(),
+    });
+    const testSchema = z.object({
+      fooString: z.string(),
+      fooNum: z.number(),
+      fooObj: z.object({
+        fooObjOne: z.string(),
+        fooObjTWo: z.number(),
+      }),
+      fooStringArray: z.array(z.string()),
+      fooArray: z.array(testArrSchema),
+    });
+
+    console.log(testSchema.parse({ fooObj: {}, fooStringArray: [1] }));
+  } catch (e) {
+    console.error(e);
+    const output = parseDynamicErrors(e);
+    console.warn(output);
+  }
+
   return (
     <div className="grid grid-cols-[1fr_1fr] grid-rows-[auto_1fr_auto] gap-4 items-center h-full">
       <h1 className="col-span-2 self-end">Zod + Zustand Simple Form</h1>
--- src/util/parseZodErrors.ts
+++ src/util/parseZodErrors.ts
@@ -1,4 +1,4 @@
-import type { z } from "zod";
+import { ZodError, z } from "zod";
 
 export type ZodErrors<T extends Record<string, unknown>> = Partial<{
   [key in keyof T]?: string;
@@ -16,3 +16,107 @@ const parseZodErrors = <T extends Record<string, unknown>>(
 };
 
 export default parseZodErrors;
+
+const testArrSchema = z.object({
+  testArrFoo: z.string(),
+  testArrBar: z.string(),
+});
+
+const testSchema = z.object({
+  fooString: z.string(),
+  fooNum: z.number(),
+  fooObj: z.object({
+    fooObjOne: z.string(),
+    fooObjTWo: z.number(),
+  }),
+  fooStringArray: z.array(z.string()),
+  fooArray: z.array(testArrSchema),
+});
+
+type TestSchema = z.infer<typeof testSchema>;
+
+type DynamicErrorsShape<T extends Record<string, unknown>> = Partial<{
+  [key in keyof T]?: T[key] extends string | number
+    ? {
+        message: string;
+      }
+    : T[key] extends Array<string | number>
+      ? {
+          message: string;
+          [arrayKey: number]: {
+            message: string;
+          };
+        }
+      : T[key] extends Array<Record<string, unknown>>
+        ? {
+            message: string;
+            testType: T[key];
+            [arrayKey: number]: DynamicErrorsShape<T[key][0]>;
+          }
+        : {
+            message: string;
+          };
+}>;
+
+type myTestType = DynamicErrorsShape<TestSchema>;
+console.log({} as myTestType);
+
+export const parseDynamicErrors = <T extends Record<string, unknown>>(
+  err: z.ZodError,
+): DynamicErrorsShape<T> => {
+  let formattedErrors: DynamicErrorsShape<T> = {};
+  err.errors.forEach((zodError) => {
+    console.log(zodError);
+    if (zodError.path.length === 1) {
+      formattedErrors = {
+        ...formattedErrors,
+        [zodError.path[0] as keyof T]: { message: zodError.message },
+      };
+    } else if (zodError.path.length === 2) {
+      if (typeof zodError.path[1] === "string") {
+        formattedErrors = {
+          ...formattedErrors,
+          [zodError.path[0] as keyof T]: {
+            ...formattedErrors[zodError.path[0] as keyof T],
+            message: `Error in '${zodError.path[0]}' child`,
+            [zodError.path[1]]: {
+              message: zodError.message,
+            },
+          },
+        };
+      } else if (typeof zodError.path[1] === "number") {
+        formattedErrors = {
+          ...formattedErrors,
+          [zodError.path[0] as keyof T]: {
+            ...formattedErrors[zodError.path[0] as keyof T],
+            message: `Error in '${zodError.path[0]} chilld'`,
+            [zodError.path[1]]: {
+              message: zodError.message,
+            },
+          },
+        };
+      }
+    } else if (zodError.path.length === 3) {
+      if (typeof zodError.path[1] === "number") {
+        formattedErrors = {
+          ...formattedErrors,
+          [zodError.path[0] as keyof T]: {
+            ...formattedErrors[zodError.path[0] as keyof T],
+            message: `Error in '${zodError.path[0]}' child`,
+            [zodError.path[1]]: {
+              // @ts-expect-error This works, but struggles to infer the type
+              ...((formattedErrors[zodError.path[0] as keyof T] || {})[
+                zodError.path[1].toString()
+              ] || {}),
+              [zodError.path[2]]: {
+                message: zodError.message,
+              },
+            },
+          },
+        };
+      }
+    }
+  });
+
+  return formattedErrors;
+};
